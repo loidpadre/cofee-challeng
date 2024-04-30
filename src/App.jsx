3947ee40804198dbc68614069ef510898fb5b894
loidpadre <loidpadre@gmail.com>
2024-04-30 21:18:09 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import IMG from './assets/bg.jpg'
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+	const [data, setData] = useState([])
+
+	const getData = async () => {
+		try {
+			const response = await fetch(
+				'https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json'
+			)
+			const request = await response.json()
+			setData(request)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	useEffect(() => {
+		getData()
+	}, [])
+	return (
+		<>
+			<div>
+				<img src={IMG} alt="bg-cofee" />
+				<div className="w-[1110px] bg-[#1B1D1F] text-white rounded-xl p-8 mx-auto -top-20 relative">
+					<div className="text-center w-1/2 m-auto ">
+						<h1 className="font-bold text-4xl">Our collection</h1>
+						<p className="mt-5 text-gray-400">
+							Começando pela acidez, seguindo pelo aroma e depois
+							pelo sabor. Por exemplo, você pode descrever o café
+							como – um com sabor forte e brilhante, com notas
+							cítricas e de caramelo. Geralmente, não há uma
+							maneira de descrever uma
+						</p>
+						<div className='flex gap-5 items-center justify-center mt-5'>
+							<button className='bg-gray-400 py-2 px-5 rounded-xl font-semibold'>All product</button>
+							<button className='font-semibold'>Avaliable now</button>
+						</div>
+					</div>
+					<div className='grid grid-cols-3 mt-10 gap-y-20'>
+						{data.map((info) => (
+							<Card info={info} />
+						))}
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+import { FaStar } from 'react-icons/fa'
+export default function Card({ info }) {
+	const { name, price, image, rating, votes, popular } = info
+	return (
+		<div>
+			<div className="w-[250px]">
+				<div className='relative'>
+					<img className="rounded-2xl mb-2" src={image} alt="" />
+                    {
+                        popular? <p className='absolute top-2 left-2 bg-[#F6C768] py-1 px-2 rounded-xl  text-zinc-800'>Popular</p>: ""
+                    }
+				</div>
+				<div className="flex justify-between">
+					<h1 className="font-semibold">{name}</h1>
+					<p className="bg-[#BEE3CC] py-0 text-black font-semibold px-2 rounded">
+						{price}
+					</p>
+				</div>
+				<div className="flex items-center gap-2">
+					<div className="flex gap-2 items-center">
+						<FaStar color="yellow" />
+						<p className="font-semibold">{rating}</p>
+					</div>
+					<p className="text-gray-500 font-semibold">{`( ${votes} Votes )`}</p>
+				</div>
+			</div>
+		</div>
+	)
+}
